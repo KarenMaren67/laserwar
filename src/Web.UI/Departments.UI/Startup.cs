a3b9f3ce3a91fb4ceb7bb747597b4377bd2169b6
KVoskanyan <k.voskanyan@structura.pro>
2020-03-06 12:21:04 +0300

    Промежуточные изменения по WEB UI

--- src/Web.UI/Departments.UI/Startup.cs
+++ src/Web.UI/Departments.UI/Startup.cs
@@ -2,9 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application;
+using Application.Abstraction.DAL;
+using Application.Services;
+using Application.Validation.Department;
+using Application.Validation.Emplyee;
+using AutoMapper;
+using DAL.EF;
+using DataAccessLayer.DAL.EF;
+using FluentValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +34,24 @@ namespace Departments.UI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddEntityFrameworkNpgsql()
+                 .AddDbContext<EFDataContext>(options =>
+                                                 options.UseNpgsql(
+                                                     Configuration.GetConnectionString("PostgreDbConnectionString")));
+
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+
+            services.AddScoped(typeof(IConnectionStringProvider<string>), typeof(PostgreSqlConnectionStringProvider));
+            services.AddScoped(typeof(IUnitOfWorkFactory), typeof(EFUnitOfWorkFactory));
+            services.AddScoped(typeof(IService<Department>), typeof(DepartmentsService));
+            services.AddScoped(typeof(IService<Employee>), typeof(EmployeeService));
+            services.AddScoped(typeof(AbstractValidator<Department>), typeof(DepartmentValidator));
+            services.AddScoped(typeof(AbstractValidator<Employee>), typeof(EmployeeValidator));
+
+            services.AddAutoMapper(
+                typeof(Program).Assembly,
+                typeof(ApplicationModule).Assembly,
+                typeof(DALModule).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +64,6 @@ namespace Departments.UI
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
@@ -44,13 +71,20 @@ namespace Departments.UI
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllerRoute(
+            //        name: "default",
+            //        pattern: "{controller=Home}/{action=Index}/{id?}");
+            //});
+
+            app.UseMvc(routes =>
             {
-                endpoints.MapControllerRoute(
+                routes.MapRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
--- src/Web.UI/Departments.UI/wwwroot/js/site.js
+++ src/Web.UI/Departments.UI/wwwroot/js/site.js
@@ -1,4 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//$(function () {
+//    $(document).ready(function () {
+//        var toggler = document.getElementsByClassName("caret");
+//        var i;
 
-// Write your JavaScript code.
+//        for (i = 0; i < toggler.length; i++) {
+//            toggler[i].addEventListener("click", function () {
+//                this.parentElement.querySelector(".nested").classList.toggle("active");
+//                this.classList.toggle("caret-down");
+//            });
+//        }
+//    });
+//});
+
+function UnHide(eThis) {
+    if (eThis.innerHTML.charCodeAt(0) == 9658) {
+        eThis.innerHTML = '&#9660;'
+        eThis.parentNode.parentNode.parentNode.className = '';
+    } else {
+        eThis.innerHTML = '&#9658;'
+        eThis.parentNode.parentNode.parentNode.className = 'cl';
+    }
+    return false;
+}
