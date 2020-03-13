c03efcf4587c8e4caaf3f0058ddeb6cb145aa1d0
KVoskanyan <k.voskanyan@structura.pro>
2020-03-13 12:31:56 +0300

    Дроаботка дерева

--- src/Web.UI/Departments.UI/Models/Departments/DepartmentsTreeItemViewModel.cs
+++ src/Web.UI/Departments.UI/Models/Departments/DepartmentsTreeItemViewModel.cs
@@ -7,17 +7,23 @@ namespace Departments.UI.Models.Departments
 {
     public class DepartmentsTreeItemViewModel
     {
-        public DepartmentsTreeItemViewModel(int id, string name, List<DepartmentsTreeItemViewModel> childsList)
+        public DepartmentsTreeItemViewModel(int id, string name, IEnumerable<DepartmentsTreeItemViewModel> childsList, DepartmentsTreeItemViewModel parent, bool isSelected = false)
         {
             Id = id;
             Name = name;
             ChildDepartments = childsList ?? new List<DepartmentsTreeItemViewModel>();
+            Parent = parent;
+            IsSelected = isSelected;
         }
 
         public int Id { get; }
 
         public string Name { get;  }
 
-        public List<DepartmentsTreeItemViewModel> ChildDepartments { get;  }
+        public IEnumerable<DepartmentsTreeItemViewModel> ChildDepartments { get; set;}
+
+        public DepartmentsTreeItemViewModel Parent { get; set; }
+
+        public bool IsSelected { get; set;}
     }
 }
